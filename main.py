8290e88212d20de70b1019dff128883b7258f6a3
TarinNoovare <tarin.no@tnis.com>
2021-02-01 15:14:34 +0700

    Add item_id path in main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+test_gay = FastAPI()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: float):
+    return {"item_id": item_id}
+
